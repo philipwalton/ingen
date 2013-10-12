8245baa0decf646ef6b338a7136ce103c1d462af
Philip Walton <philip@philipwalton.com>
2013-10-12 11:11:06 -0700

    Add support for nested layouts.

--- plugins/handlebars-extensions.js
+++ plugins/handlebars-extensions.js
@@ -2,7 +2,7 @@ var fs = require('fs')
   , Handlebars = require('handlebars')
 
 Handlebars.registerHelper('include', function(filename) {
-  var source = fs.readFileSync(filename, 'utf8')
+  var source = fs.readFileSync('_includes/' + filename, 'utf8')
     , template = Handlebars.compile(source)
   return new Handlebars.SafeString(template(this))
 })
