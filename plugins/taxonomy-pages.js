4759da8696deadbd7966302c0196adfc02c67a59
Philip Walton <philip@philipwalton.com>
2013-10-27 22:49:15 -0700

    Add the page template object instead of the actual instance to the site's template data.

--- plugins/taxonomy-pages.js
+++ plugins/taxonomy-pages.js
@@ -4,6 +4,13 @@ site.on('beforeBuild', function() {
 
   debugger
 
+  // TODO: add the ability to create pages without a file
+  // new Page({
+  //   title: taxonomyName,
+  //   content: '',
+  //   permalink: taxonomyName + '.html'
+  // })
+
   // create a new Page instance that has the taxonomy data
 
 })
