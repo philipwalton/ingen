8bf9c87f63903e9e8dfff91beb2fe26ba5752b38
Philip Walton <philip@philipwalton.com>
2013-10-29 19:25:07 -0700

    First stab at creating taxonomy pages.

--- plugins/taxonomy-pages.js
+++ plugins/taxonomy-pages.js
@@ -1,16 +1,29 @@
-var site = require('../lib/site')
+var fs = require('fs')
+  , site = require('../lib/site')
+  , Page = require('../lib/page')
+  , File = require('../lib/file')
+  , natural = require('natural')
+  , inflector = new natural.NounInflector()
 
 site.on('beforeBuild', function() {
 
-  debugger
 
-  // TODO: add the ability to create pages without a file
-  // new Page({
-  //   title: taxonomyName,
-  //   content: '',
-  //   permalink: taxonomyName + '.html'
-  // })
 
-  // create a new Page instance that has the taxonomy data
+  site.config.taxonomies.forEach(function(type) {
+    var typePlural = inflector.pluralize(type)
+
+    Object.keys(site._taxonomies[typePlural]).forEach(function(value) {
+
+      if (fs.existsSync('_layouts/' + type.toLowerCase() + '.html')) {
+        new Page({
+          content: "This is the " + type + " taxonomy content.",
+          title: value,
+          permalink: (typePlural + '/' + value).toLowerCase(),
+          layout: type.toLowerCase()
+        })
+      }
+
+    })
+  })
 
 })
