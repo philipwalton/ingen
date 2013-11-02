4ea610a2585a7d5efe41222347aceb08e6ff85ef
Philip Walton <philip@philipwalton.com>
2013-11-02 15:03:43 -0700

    Replace the include helper with native handlebars partials.

--- plugins/include.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs')
-  , Handlebars = require('handlebars')
-
-Handlebars.registerHelper('include', function(filename) {
-  var source = fs.readFileSync('_includes/' + filename, 'utf8')
-    , template = Handlebars.compile(source)
-  return new Handlebars.SafeString(template(this))
-})
