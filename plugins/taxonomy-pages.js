38e5933abe9ebc244118c3ef103ccc0fbdff7428
Philip Walton <philip@philipwalton.com>
2013-11-02 13:43:38 -0700

    Add an eachTaxonomy convenience method.

--- plugins/taxonomy-pages.js
+++ plugins/taxonomy-pages.js
@@ -6,26 +6,25 @@ var fs = require('fs')
   , inflector = new natural.NounInflector()
   , _ = require('lodash')
 
+
+var layoutExists = _.memoize(function(layout) {
+  return fs.existsSync('_layouts/' + layout + '.html')
+})
+
 site.on('beforeBuild', function() {
 
-  _.each(site._taxonomies, function(taxonomyValues, taxonomyTypePlural) {
-
-    var taxonomyType = inflector.singularize(taxonomyTypePlural)
-
-    _.each(taxonomyValues, function(taxonomyValue) {
-      if (fs.existsSync('_layouts/' + taxonomyType.toLowerCase() + '.html')) {
-        var obj = {
-          content: '',
-          title: taxonomyValue,
-          permalink: (taxonomyTypePlural + '/' + taxonomyValue).toLowerCase(),
-          layout: taxonomyType.toLowerCase()
-        }
-        // add the taxonomy type and value to the page data
-        obj[taxonomyType] = taxonomyValue
-        new Page(obj)
+  site.eachTaxonomy(function(taxonomyValue, taxonomyType, taxonomyTypePlural) {
+    if (layoutExists(taxonomyType.toLowerCase())) {
+      var obj = {
+        content: '',
+        title: taxonomyValue,
+        permalink: (taxonomyTypePlural + '/' + taxonomyValue).toLowerCase(),
+        layout: taxonomyType.toLowerCase()
       }
-
-    })
+      // add the taxonomy type and value to the page data
+      obj[taxonomyType] = taxonomyValue
+      new Page(obj)
+    }
   })
 
 })
