31759b0ff0f49e7ce4b0a7c558aff427b7977748
Philip Walton <philip@philipwalton.com>
2013-11-10 22:26:36 -0800

    Abstract common functionality in the post iterators.

--- plugins/post-iterators.js
+++ plugins/post-iterators.js
@@ -14,30 +14,20 @@ function capitalize(word) {
   return word[0].toUpperCase() + word.slice(1)
 }
 
-_.each(config.postTypes, function(type) {
-
-  Handlebars.registerHelper('each' + capitalize(type), function(options) {
-
-
-    var params = _.assign({type: type}, options.hash)
-      , query = new Query(params)
-
-    // render the result
-    return _.map(query.run(), function(post) {
-      return options.fn(post.toTemplateObject())
-    }).join('')
-
-  });
-
-})
-
-
-Handlebars.registerHelper('query', function(options) {
-  var query = new Query(this.page.query)
-
-  // render the result
+function renderQuery(params, options) {
+  var query = new Query(params)
   return _.map(query.run(), function(post) {
     return options.fn(post.toTemplateObject())
   }).join('')
+}
 
+_.each(config.postTypes, function(type) {
+  Handlebars.registerHelper('each' + capitalize(type), function(options) {
+    var params = _.assign({type: type}, options.hash)
+    return renderQuery(params, options)
+  });
+})
+
+Handlebars.registerHelper('query', function(options) {
+  return renderQuery(this.page.query, options)
 })
