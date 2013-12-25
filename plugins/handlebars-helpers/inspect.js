92422f598a93a2a21c4a8cf55e79bbf6f76a4ab8
Philip Walton <philip@philipwalton.com>
2013-12-24 22:48:51 -0600

    Rename the debug helper to inspect to avoid conficts with swag.

--- plugins/handlebars-helpers/inspect.js
+++ plugins/handlebars-helpers/inspect.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 var Handlebars = require('handlebars')
 
-Handlebars.registerHelper('debug', function(obj) {
+Handlebars.registerHelper('inspect', function(obj) {
   debugger
   return obj
 })
