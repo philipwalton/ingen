501830b81b089c1dc0c44f4c34af4a8dea44f700
Philip Walton <philip@philipwalton.com>
2013-12-26 14:46:00 -0600

    Simplify the markdown convertion to just use the default options. Let sites override in the future.

--- plugins/markdown.js
+++ plugins/markdown.js
@@ -1,18 +1,10 @@
 var marked = require('marked')
-var hljs = require("highlight.js")
 var events = require('../lib/events')
-
-
-marked.setOptions({
-  highlight: function(code, lang) {
-    return lang
-      ? hljs.highlight(lang, code).value
-      : hljs.highlightAuto(code).value
-  }
-})
 
 events.on('afterRenderContent', function(page) {
   if (page.extension == '.md') {
+    // TODO: changing the extention should be automatically done
+    // by the Permalink object.
     page.permalink = page.permalink.replace(/\.md$/, '.html')
     page.content = marked(page.content)
   }
