521af49cfc60e75ac1943b40677c7581e337e3e8
Philip Walton <philip@philipwalton.com>
2013-12-30 16:27:20 -0600

    Add the layout directory as a config option.

--- plugins/taxonomy-pages.js
+++ plugins/taxonomy-pages.js
@@ -3,12 +3,13 @@ var natural = require('natural')
 var inflector = new natural.NounInflector()
 var _ = require('lodash-node/modern')
 
+var config = require('../lib/config')
 var events = require('../lib/events')
 var Page = require('../lib/page')
 var Taxonomy = require('../lib/taxonomy')
 
 var layoutExists = function(layout) {
-  return fs.existsSync('_layouts/' + layout + '.html')
+  return fs.existsSync(config.layoutsDirectory + '/' + layout + '.html')
 }
 
 events.on('beforeBuild', function() {
