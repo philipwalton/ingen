47d5f1350c3a816a6a6c8d5db9a8f9a3f5080f99
Philip Walton <philip@philipwalton.com>
2013-12-31 14:38:14 -0600

    Expose modules to site authors.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./lib/site')
+var site = require('./lib/site')
+
+// expose local modules
+site.config = require('./lib/config')
+site.events = require('./lib/events')
+site.Post = require('./lib/Post')
+site.Page = require('./lib/Page')
+site.Query = require('./lib/Query')
+site.Taxonomy = require('./lib/Taxonomy')
+site.Permalink = require('./lib/Permalink')
+
+// expose instances of npm modules
+site.Handlebars = require('handlebars')
+
+module.exports = site
