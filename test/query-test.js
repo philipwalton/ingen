8bda9d79c0eebc20fbdf47b70a7e8a0ab55a2ef1
Philip Walton <philip@philipwalton.com>
2014-02-15 17:33:34 -0800

    Update the query tests to work with the new post fixtures.

--- test/query-test.js
+++ test/query-test.js
@@ -1,8 +1,27 @@
 var fs = require('fs-extra')
 var expect = require('chai').expect
-var Query = require('../lib/query')
-var posts = fs.readJSONSync('test/fixtures/posts.json')
 var _ = require('lodash-node/modern')
+
+var Query = require('../lib/query')
+
+var posts = fs.readJSONSync('test/fixtures/posts.json')
+var recipes = posts.slice(0,5)
+var articles = posts.slice(5)
+
+var recipesUnsorted = [
+  recipes[1],
+  recipes[3],
+  recipes[4],
+  recipes[2],
+  recipes[0]
+]
+var articlesUnsorted = [
+  articles[1],
+  articles[3],
+  articles[4],
+  articles[2],
+  articles[0]
+]
 
 describe('Query', function() {
 
@@ -10,7 +29,7 @@ describe('Query', function() {
 
     it('can filter a query by post type', function() {
       var q = new Query({type:'article'}, posts)
-      expect(q.filter().posts).to.deep.equal(posts.slice(4, 8))
+      expect(q.filter().posts).to.deep.equal(articles)
     })
 
     it('can filter a query by a single taxonomy', function() {
@@ -19,24 +38,26 @@ describe('Query', function() {
         posts[0],
         posts[1],
         posts[4],
-        posts[5]
+        posts[5],
+        posts[6],
+        posts[9]
       ])
     })
 
     it('can filter a query by multiple taxonomies', function() {
       var q = new Query({tag:'red', author:'john'}, posts)
       expect(q.filter().posts).to.deep.equal([
-        posts[4]
+        posts[5]
       ])
     })
 
     it('can filter a query by both type and taxonomy', function() {
       var q = new Query({type:'article', tag:'blue', author:'philip'}, posts)
       expect(q.filter().posts).to.deep.equal([
-        posts[5],
-        posts[7]
+        posts[6],
+        posts[8]
       ])
-      q = new Query({type:'page', author:'philip'}, posts)
+      q = new Query({type:'recipe', author:'philip'}, posts)
       expect(q.filter().posts).to.deep.equal([])
     })
 
@@ -50,33 +71,18 @@ describe('Query', function() {
   describe('#sort', function() {
 
     it('can sort posts by title', function() {
-      var q = new Query({sortBy:'title'}, posts.slice(0,4))
-      expect(q.sort().posts).to.deep.equal([
-        posts[0],
-        posts[3],
-        posts[1],
-        posts[2]
-      ])
+      var q = new Query({sortBy:'title'}, recipesUnsorted)
+      expect(q.sort().posts).to.deep.equal(recipes)
     })
 
     it('can sort posts by date', function() {
-      var q = new Query({sortBy:'date'}, posts.slice(4))
-      expect(q.sort().posts).to.deep.equal([
-        posts[7],
-        posts[5],
-        posts[4],
-        posts[6]
-      ])
+      var q = new Query({sortBy:'date'}, articlesUnsorted)
+      expect(q.sort().posts).to.deep.equal(articles)
     })
 
     it('sorts by date by default', function() {
-      var q = new Query({}, posts.slice(4))
-      expect(q.sort().posts).to.deep.equal([
-        posts[7],
-        posts[5],
-        posts[4],
-        posts[6]
-      ])
+      var q = new Query({}, articlesUnsorted)
+      expect(q.sort().posts).to.deep.equal(articles)
     })
 
   })
@@ -84,20 +90,20 @@ describe('Query', function() {
   describe('#order', function() {
 
     it('can put posts in ascending order', function() {
-      var q = new Query({order:'asc'}, posts.slice(0,4))
-      expect(q.order().posts).to.deep.equal(posts.slice(0,4))
+      var q = new Query({order:'asc'}, recipesUnsorted)
+      expect(q.order().posts).to.deep.equal(recipes)
     })
 
     it('can put posts in descending order', function() {
-      var q = new Query({order:'desc'}, posts.slice(0,4))
-      expect(q.order().posts).to.deep.equal(posts.slice(0,4).reverse())
+      var q = new Query({order:'desc'}, recipesUnsorted)
+      expect(q.order().posts).to.deep.equal(recipes.reverse())
     })
 
     it('puts posts in ascending order by default unless the posts are being sorted by date', function() {
-      var q = new Query({}, posts.slice(0,4))
-      expect(q.order().posts).to.deep.equal(posts.slice(0,4))
-      q = new Query({sortBy:'date'}, posts.slice(0,4))
-      expect(q.order().posts).to.deep.equal(posts.slice(0,4).reverse())
+      var q = new Query({}, recipesUnsorted)
+      expect(q.order().posts).to.deep.equal(recipes)
+      q = new Query({sortBy:'date'}, recipesUnsorted)
+      expect(q.order().posts).to.deep.equal(recipes.reverse())
     })
 
   })
@@ -106,25 +112,19 @@ describe('Query', function() {
 
     it('can limit posts by the specified limit amount', function() {
       var q = new Query({
-        limit: 3
-      }, posts)
-      expect(q.run()).to.deep.equal([
-        posts[6],
-        posts[4],
-        posts[5]
-      ])
+        limit: 3,
+        sortBy: 'title'
+      }, recipes)
+      expect(q.run()).to.deep.equal(recipes.slice(0,3))
     })
 
     it('can paginate posts via the specified page and limit', function() {
       var q = new Query({
         limit: 3,
+        sortBy: 'title',
         page: 2
-      }, posts)
-      expect(q.run()).to.deep.equal([
-        posts[7],
-        posts[3],
-        posts[2]
-      ])
+      }, recipes)
+      expect(q.run()).to.deep.equal(recipes.slice(3))
     })
 
   })
@@ -141,7 +141,7 @@ describe('Query', function() {
         limit: 1,
         page: 2
       }, posts)
-      expect(q.run()).to.deep.equal([posts[5]])
+      expect(q.run()).to.deep.equal([articles[3]])
     })
 
   })
