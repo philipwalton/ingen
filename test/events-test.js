b33328a9d0c8b36b1db6cf338bde9e2c0f3ae37a
Philip Walton <philip@philipwalton.com>
2014-02-15 21:24:13 -0800

    Add events tests.

---
+++ test/events-test.js
@@ -0,0 +1,10 @@
+var EventEmitter = require('events').EventEmitter
+var expect = require('chai').expect
+
+var events = require('../lib/events')
+
+describe('events', function() {
+  it('is an event emitter', function() {
+    expect(events instanceof EventEmitter).to.be.ok
+  })
+})
