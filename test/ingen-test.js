06cc42485ef230aa5a2e29857415c9de3025a511
Philip Walton <philip@philipwalton.com>
2014-02-18 22:16:05 -0800

    Rename ingen build test to make more clear the testing difference between site and the executable.

--- test/ingen-test.js
+++ test/ingen-test.js
@@ -59,3 +59,4 @@ describe('ingen', function() {
   })
 
 })
+
---
+++ test/site-test.js
@@ -0,0 +1,17 @@
+var fs = require('fs-extra')
+var path = require('path')
+var expect = require('chai').expect
+var _ = require('lodash-node/modern')
+
+describe('site', function() {
+
+  describe('build', function() {
+    it('should build the site')
+  })
+
+  describe('serve', function() {
+    it('should serve the site')
+  })
+
+
+})
