62e969b88ebc1233e2e9c88ef6ef99d0afdefd82
Philip Walton <philip@philipwalton.com>
2014-03-01 22:10:02 -0800

    Update post manager code and tests.

--- test/post-manager-test.js
+++ test/post-manager-test.js
@@ -2,22 +2,10 @@ var fs = require('fs-extra');
 var expect = require('chai').expect;
 // var _ = require('lodash-node/modern')
 
-var PostManager = require('../lib/post-manager')
+var PostManager = require('../lib/post-manager');
 var posts = fs.readJSONSync('test/fixtures/posts.json');
 
 describe('PostManager', function() {
-
-  describe('new', function() {
-    it('creates a new post manager instance with a posts array'
-        + 'and a taxonomy organizer', function() {
-
-      var pm = new PostManager(['tag', 'author']);
-      expect(pm.posts).to.deep.equal([]);
-      expect(pm.taxonomyTypes).to.deep.equal(['tag', 'author']);
-      expect(pm.taxonomies).to.deep.equal({'tag': {}, 'author': {}});
-
-    });
-  });
 
   describe('#add', function() {
     it('can add a post to the posts array', function() {
@@ -26,29 +14,52 @@ describe('PostManager', function() {
       var article = posts[8];
 
       pm.add(recipe);
-      expect(pm.posts).to.deep.equal([recipe]);
-      expect(pm.taxonomies).to.deep.equal({
-        'author': {},
-        'tag': {
-          'red': [recipe],
-          'green': [recipe]
-        }
-      });
+      expect(pm.all()).to.deep.equal([recipe]);
 
       pm.add(article);
-      expect(pm.posts).to.deep.equal([recipe, article]);
-      expect(pm.taxonomies).to.deep.equal({
-        'author': {
-          'philip': [article]
-        },
-        'tag': {
-          'red': [recipe],
-          'green': [recipe, article],
-          'blue': [article]
-        }
-      });
+      expect(pm.all()).to.deep.equal([recipe, article]);
+    });
+  });
+
+  describe('#all', function() {
+    it('returns an array of all posts', function() {
+      var pm = new PostManager(['tag', 'author']);
+      var recipe = posts[0];
+      var article = posts[8];
+
+      pm.add(recipe);
+      pm.add(article);
+
+      expect(pm.all()).to.deep.equal([recipe, article]);
 
     });
-  })
+  });
+
+  describe('#getByTaxonomy', function() {
+    it('returns an array of posts with the specified '
+        + 'taxonomy name and value', function() {
+
+      var pm = new PostManager(['tag', 'author']);
+      posts.forEach(pm.add, pm);
+
+      expect(pm.getByTaxonomy('author', 'philip')).to.deep.equal([
+        posts[5],
+        posts[6],
+        posts[8],
+        posts[9]
+      ]);
+
+      expect(pm.getByTaxonomy('tag', 'green')).to.deep.equal([
+        posts[0],
+        posts[2],
+        posts[3],
+        posts[5],
+        posts[7],
+        posts[8]
+      ]);
+
+    });
+
+  });
 
 });
