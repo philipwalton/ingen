8e1631182ce58fd307600ae5de97c35d36f581f3
Philip Walton <philip@philipwalton.com>
2014-07-11 08:19:38 -0700

    Squash.

--- plugins/markdown-converter.js
+++ plugins/markdown-converter.js
@@ -4,7 +4,7 @@ module.exports = function() {
 
   // This could be for a post or a page.
   this.events.on('afterRenderContent', function(p) {
-    if (p.data.format == 'markdown') {
+    if (p.format == 'markdown') {
       p.content = marked(p.content);
     }
   });
